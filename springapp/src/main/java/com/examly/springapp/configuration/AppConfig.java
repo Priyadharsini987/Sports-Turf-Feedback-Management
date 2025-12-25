91d76ab9ae90d1ee86f314a19025023abc2bb049
727724eucs195-dotcom <727724eucs195@skcet.ac.in>
2025-12-25 12:23:42 +0000

    completed sucessfully

---
+++ springapp/src/main/java/com/examly/springapp/configuration/AppConfig.java
@@ -0,0 +1,10 @@
+package com.examly.springapp.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@EnableAspectJAutoProxy
+@Configuration
+public class AppConfig {
+
+}
